27e2ebbb6f06c60cca3c60ece809b94767e5061f
Patrick Issler <patrickissler1234@gmail.com>
2021-06-17 07:44:39 -0300

    editando conta , falta colocar pro login pegar ela.

--- lib/globals/metodos/Login.dart
+++ lib/globals/metodos/Login.dart
@@ -12,13 +12,27 @@ Future<Conta> realizarLogin(context) async {
 
   if (response.statusCode == 200) {
     var dadosJson = json.decode(response.body);
-    Conta conta = Conta(dadosJson["conta"]["id"], dadosJson["conta"]["conta"].toString(),
-        dadosJson["conta"]["senha"].toString(), dadosJson["conta"]["mfa"].toString());
+    Conta conta = Conta(
+        dadosJson["id"],
+        dadosJson["nome"].toString(),
+        dadosJson["cpf"].toString(),
+        dadosJson["email"].toString(),
+        dadosJson["dataNascimento"].toString(),
+        dadosJson["genero"].toString(),
+        dadosJson["telefone"].toString(),
+        dadosJson["cargo"].toString(),
+        dadosJson["empresa"].toString(),
+        dadosJson["endereco"].toString(),
+        dadosJson["conta"]["id"],
+        dadosJson["conta"]["conta"].toString(),
+        dadosJson["conta"]["senha"].toString(),
+        dadosJson["conta"]["mfa"].toString());
     variaveis.conta = conta;
     return conta;
   }
   if (response.statusCode == 400) {
-    Conta conta = new Conta(null, null, null, null,null,null,null,null,null,null,null,null,null,null);
+    Conta conta = new Conta(null, null, null, null, null, null, null, null,
+        null, null, null, null, null, null);
     return conta;
   } else {
     Navigator.pop(context);
